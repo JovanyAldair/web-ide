86fce75e859facf123a040f66bf1ecf5a468c211
JovanyAldair <azevedojovani90@gmail.com>
2024-08-03 21:58:26 +0100

    features removed

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8003
 const ide = require("./routes/ide")
-const admin = require("./routes/admin")
-const users = require("./routes/users")
-const course = require("./routes/course")
 const ejs = require('ejs')
 
 
@@ -14,16 +11,12 @@ app.set('view engine', 'ejs')
 app.set('views', './views')
 app.use(express.static('public'))
 
-// --------------- rotas --------------
+// --------------- routes --------------
 
 
 app.use('/ide', ide)
-app.use('/admin', admin)
-app.use('/users', users)
-app.use('/course', course)
-
 
 
 app.listen(port, () => {
-    console.log('servidor rodando...')
+    console.log('The server is ')
 })
--- models/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize") 
-const sequelize = new Sequelize('web_ide_db', 'admin', 'password', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-module.exports = {Sequelize, sequelize}
